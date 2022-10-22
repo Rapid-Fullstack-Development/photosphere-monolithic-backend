0296e994e787dd64e194d0107db2a179ae97612e
Ashley Davis <ashley@codecapers.com.au>
2022-10-22 18:07:53 +1000

    Added simple HTTP POST route handler to upload an asset.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-const express = require('express');
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.post("/asset", (req, res) => {
+    
+    const fileName = req.headers["file-name"];
+    const localFileName = path.join(__dirname, "../uploads", fileName);
+    const fileWriteStream = fs.createWriteStream(localFileName);
+    req.pipe(fileWriteStream)
+        .on("error", err => {
+            console.error(`Error writing ${localFileName}`);
+            console.error(err);
+            res.sendStatus(500);
+        })
+        .on("finish", () => {
+            console.log(`Done writing ${localFileName}`);
+            res.sendStatus(200);
+        });    
 });
 
 app.listen(port, () => {
